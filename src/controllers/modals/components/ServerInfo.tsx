7e9a845eb9a66d68a4379e79262e442ffdc87835
Asraye <asrayeofficial@gmail.com>
2026-01-11 20:10:54 +1100

    feat: improve server info

--- src/controllers/modals/components/ServerInfo.tsx
+++ src/controllers/modals/components/ServerInfo.tsx
@@ -1,12 +1,10 @@
-import { X } from "@styled-icons/boxicons-regular";
-
+import { Flag, X } from "@styled-icons/boxicons-regular";
 import { Text } from "preact-i18n";
-
 import { Column, H1, IconButton, Modal, Row } from "@revoltchat/ui";
+import { useApplicationState } from "../../../mobx/State";
 
 import Markdown from "../../../components/markdown/Markdown";
 import { useClient } from "../../client/ClientController";
-import { report } from "../../safety";
 import { modalController } from "../ModalController";
 import { ModalProps } from "../types";
 
@@ -17,19 +15,38 @@ export default function ServerInfo({
     const client = useClient();
     const isOwner = server.owner === client.user?._id;
 
-    const actions = [
-        {
+    const state = useApplicationState();
+
+    const hasUnreads = server.channel_ids.some((id) => {
+        const channel = client.channels.get(id);
+        return channel?.isUnread(state.notifications);
+    });
+    const actions = [];
+
+    if (hasUnreads) {
+        actions.push({
             onClick: () => {
-                modalController.push({
-                    type: "server_identity",
-                    member: server.member!,
-                });
+                client.unreads?.markMultipleRead(server.channel_ids);
+                server.ack();
+                modalController.close();
                 return true;
             },
-            children: "Edit Identity",
+            children: <Text id="app.context_menu.mark_as_read" />,
             palette: "primary",
+        });
+    }
+
+    actions.push({
+        onClick: () => {
+            modalController.push({
+                type: "server_identity",
+                member: server.member!,
+            });
+            return true;
         },
-    ];
+        children: "Edit Identity",
+        palette: "primary",
+    });
 
     if (!isOwner) {
         actions.push({
@@ -45,26 +62,24 @@ export default function ServerInfo({
         });
     }
 
-    actions.push({
-        onClick: () => {
-            modalController.push({
-                type: "report",
-                target: server,
-            });
-            return true;
-        },
-        children: <Text id="app.special.modals.actions.report" />,
-        palette: "error",
-    });
+    const openReport = () => {
+        modalController.push({
+            type: "report",
+            target: server,
+        });
+    };
 
     return (
         <Modal
             {...props}
             title={
-                <Row centred>
+                <Row centred gap="md">
                     <Column grow>
                         <H1>{server.name}</H1>
                     </Column>
+                    <IconButton onClick={openReport}>
+                        <Flag size={24} />
+                    </IconButton>
                     <IconButton onClick={modalController.close}>
                         <X size={36} />
                     </IconButton>
