94375e3c6d9750e1b8133ea50db30b49ae65be1d
Asraye <asrayeofficial@gmail.com>
2026-02-02 04:26:02 +1100

    feat: add new appearance setting

--- src/components/settings/appearance/AppearanceOptions.tsx
+++ src/components/settings/appearance/AppearanceOptions.tsx
@@ -26,6 +26,14 @@ export default function AppearanceOptions() {
                     <Text id="app.settings.pages.appearance.appearance_options.show_send_desc" />
                 }
             />
+            {/* Option to hide blocked messages */}
+            <ObservedInputElement
+                type="checkbox"
+                value={() => settings.get("appearance:show_blocked") ?? true}
+                onChange={(v) => settings.set("appearance:show_blocked", v)}
+                title="Show Blocked Messages"
+                description="Toggle whether messages from blocked users are visible."
+            />
             {/* Option to always show the account creation age next to join system messages. */}
             <ObservedInputElement
                 type="checkbox"
